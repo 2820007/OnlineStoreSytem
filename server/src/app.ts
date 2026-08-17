aae2625566ff8a2bcac1ca75fc16f9e9ae3b684f
Ravi Mandal <2820007.cse@pietgroup.co.in>
2026-08-17 15:28:59 +0545

    user  auth features completed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express"
 import connectDB from "./database/connection"
+import userRouter from "./routes/userRoute"
 
 const app=express()
 connectDB()
+
+app.use(express.json())
+
+
+app.use("/api/users",userRouter)
 
 
 export default app
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,12 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
+import User from "./models/userModel";
 
-const sequelize = new Sequelize(envConfig.connectionString as string);
+export const sequelize = new Sequelize(envConfig.connectionString as string,
+      {
+    models: [User],
+  }
+);
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log("Database connected successfully");
+          await sequelize.sync();
     } catch (error) {
         console.error("Database connection failed:", error);
     }
