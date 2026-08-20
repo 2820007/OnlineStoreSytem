99b4967ebfd4a4bafd6c696fb599e0b100f61079
Ravi Mandal <2820007.cse@pietgroup.co.in>
2026-08-20 13:05:42 +0545

    order and cart controller created

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import connectDB from "./database/connection"
 import userRouter from "./routes/userRoute"
 import categoryRouter from "./routes/categoryRoute"
 import productRouter from "./routes/productRoute"
+import orderRoute from "./routes/orderRoute"
+import cartRoute from "./routes/cartRoute"
 
 const app=express()
 connectDB()
@@ -13,6 +15,8 @@ app.use(express.json())
 app.use("/api/users",userRouter)
 app.use("/api/category",categoryRouter)
 app.use("/api/product",productRouter)
+app.use("/api/order",orderRoute)
+app.use("/api/cart",cartRoute)
 
 
 
